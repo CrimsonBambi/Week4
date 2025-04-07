e1934dfc5e8a6fdadc3a999341488ace8b061286
CrimsonBambi <gerli.hirv@metropolia.fi>
2025-04-07 13:40:21 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'*/
 import './App.css'
+import Greeting from './Greeting';
+import Home from './components/Home';
 
 //const [count, setCount] = useState(0) // defaultti arvo setcount päivittää countin
 
 function App() {
   return (
-    <>
+    <> // react fragment
+      <Greeting name={123} />
       <h1>My App</h1>
+      <Greeting name='Gerli'/>
+      <Home/>
     </>
   );
 }
---
+++ src/MitenMenee.jsx
@@ -0,0 +1,6 @@
+
+function MitenMenee() {
+  return <p>Miten menee?</p>;
+}
+
+export default MitenMenee
